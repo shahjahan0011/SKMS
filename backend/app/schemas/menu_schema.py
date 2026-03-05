cd5baa2817ba697bff285d20fa46262ca00e9fdb
Khushi Kishan <khushi.kishan2004@gmail.com>
2026-03-05 13:09:17 -0800

    fixed import error

--- backend/app/schemas/menu_schema.py
+++ backend/app/schemas/menu_schema.py
@@ -1,7 +1,7 @@
 """Menu Schema"""
 from typing import List
 from pydantic import BaseModel
-from backend.app.schemas.menu_item_schema import MenuItem
+from app.schemas.menu_item_schema import MenuItem
 
 
 class Menu(BaseModel):
