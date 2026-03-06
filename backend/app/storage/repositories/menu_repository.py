1e86b068cb34d2484d14b9131acc7440fdc46812
natsofek <62151299+natsofek@users.noreply.github.com>
2026-03-05 20:03:49 -0800

    Revert "menu, restaurant,cvsStore and pylint changes"
    This reverts commit a9068d61090a97c6f399ef449205da1264ccf911.

--- backend/app/storage/repositories/menu_repository.py
+++ backend/app/storage/repositories/menu_repository.py
@@ -1,16 +1,17 @@
 """Menu repository for fetching menu data."""
+
 from typing import List, Dict
 from backend.app.storage.csv_store import CSVStore
 
 class MenuRepository:
     """Repository for fetching menu data."""
+    
     def __init__(self):
-        self.file_path = "backend/app/storage/data/menu_items.csv"
+        self.file_path = "app/storage/menus.csv"
+    
+    def get_menu_by_restaurant(self, restaurant_id: str) -> List[Dict]:
 
-    def get_by_restaurant(self, restaurant_id: str) -> List[Dict]:
-        """Fetch menu items for a specific restaurant from the CSV file."""
         all_menus = CSVStore.read_csv(self.file_path)
-        return [
-            menu for menu in all_menus 
-            if menu['restaurant_id'] == restaurant_id
-            ]
+        return [menu for menu in all_menus if menu['restaurant_id'] == restaurant_id]
+
+    
--- backend/app/storage/repositories/restaurant_repository.py
+++ backend/app/storage/repositories/restaurant_repository.py
@@ -1,13 +1,14 @@
 """Fetch restaurant data from the database."""
+
 from typing import List, Dict
 from backend.app.storage.csv_store import CSVStore
 
-# pylint: disable=too-few-public-methods
-class RestaurantRepository:
+class restaurant_repository:
     """Repository for fetching restaurant data."""
-    def __init__(self):
-        self.file_path = "backend/app/storage/data/restaurants.csv"
 
-    def get_all_restaurants(self) -> List[Dict]:
+    def __init__(self):
+        self.file_path = "app/storage/restaurants.csv"
+
+    def get_all_restaurants(self) -> List[Dict[str, str]]:
         """Fetch all restaurant data from the CSV file."""
         return CSVStore.read_csv(self.file_path)    
