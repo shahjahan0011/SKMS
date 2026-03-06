8cbe7c0f6abdc2fe828f0fd464d12b03e923e66e
natsofek <62151299+natsofek@users.noreply.github.com>
2026-03-05 18:42:43 -0800

    menu, restaurant,cvsStore and pylint changes

--- backend/app/storage/csv_store.py
+++ backend/app/storage/csv_store.py
@@ -1,22 +1,22 @@
-"""CVSStore: A simple class to handle reading CSV files and returning their contents as a list of dictionaries.
-This class provides a static method `read_csv` that takes a file path as input and returns a list of dictionaries, 
-where each dictionary represents a row in the CSV file, with the keys being the column headers and the values being the corresponding cell values.
- 
+"""CVSStore: 
 """
 import csv
 from typing import List, Dict
 
+#A simple class to handle reading CSV files and returning their contents 
+
 class CSVStore:
+# Returns a list of dictionaries, where each dictionary represents a row 
+# in the CSV file, # with the keys being the column headers and the values being
+# the corresponding cell values.
     @staticmethod
-    def read_csv(file_path: str) -> List[Dict[str, str]]:
+    def read_csv(file_path: str) -> List[Dict]:
         """Reads a CSV file and returns a list of dictionaries."""
         try:
-            with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
+            with open(file_path, mode='r', encoding='utf-8') as csvfile:
                 reader = csv.DictReader(csvfile)
-                return [row for row in reader]
-            
-        #If the file is not found, it prints an error message and returns an empty list.
+                return list(reader)         
+#If the file is not found, it prints an error message and returns an empty list.
         except FileNotFoundError:
             print(f"Error: The file {file_path} was not found.")
             return []
-        
--- backend/app/storage/repositories/restaurant_repository.py
+++ backend/app/storage/repositories/restaurant_repository.py
@@ -1,14 +1,13 @@
 """Fetch restaurant data from the database."""
-
 from typing import List, Dict
 from backend.app.storage.csv_store import CSVStore
 
-class restaurant_repository:
+# pylint: disable=too-few-public-methods
+class RestaurantRepository:
     """Repository for fetching restaurant data."""
-
     def __init__(self):
-        self.file_path = "app/storage/restaurants.csv"
+        self.file_path = "backend/app/storage/data/restaurants.csv"
 
-    def get_all_restaurants(self) -> List[Dict[str, str]]:
+    def get_all_restaurants(self) -> List[Dict]:
         """Fetch all restaurant data from the CSV file."""
         return CSVStore.read_csv(self.file_path)    
