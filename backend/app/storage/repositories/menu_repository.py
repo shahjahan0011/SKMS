34f207b3e80a0a94b2d8422ccb2dfac996953f72
natsofek <62151299+natsofek@users.noreply.github.com>
2026-03-05 20:03:49 -0800

    Revert "removing empty spaces and adding a new line."
    This reverts commit 637982f1db2193545ea4d5c8d2eed8c2a80691d8.

--- backend/app/storage/repositories/menu_repository.py
+++ backend/app/storage/repositories/menu_repository.py
@@ -5,12 +5,12 @@ from backend.app.storage.csv_store import CSVStore
 class MenuRepository:
     """Repository for fetching menu data."""
     def __init__(self):
-        self.file_path = "app/storage/data/menu_items.csv"
+        self.file_path = "backend/app/storage/data/menu_items.csv"
 
     def get_by_restaurant(self, restaurant_id: str) -> List[Dict]:
         """Fetch menu items for a specific restaurant from the CSV file."""
         all_menus = CSVStore.read_csv(self.file_path)
         return [
-            menu for menu in all_menus
+            menu for menu in all_menus 
             if menu['restaurant_id'] == restaurant_id
             ]
