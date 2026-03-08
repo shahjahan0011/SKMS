37e2953b225ff4de628b213f1d4553d0415aa4d9
Khushi Kishan <khushi.kishan2004@gmail.com>
2026-03-07 18:54:58 -0800

    updated main.py to include item listing router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,7 @@
 """FastAPI Application"""
 from fastapi import FastAPI
+from app.routers.item_listing_routers import router as item_listing_router
 
 app = FastAPI(title="SKMS Backend")
+
+app.include_router(item_listing_router)
