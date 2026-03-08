0ffc88d06da487aeb4221b83fa6520986e23f9fa
bheemstar-jpg <bheema.marabai56@gmail.com>
2026-03-07 23:00:21 -0800

    feat: add authentication router endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 """FastAPI Application"""
 from fastapi import FastAPI
-from app.routers.item_listing_routers import router as item_listing_router
+
+from app.routers.auth_router import router as auth_router
 
 app = FastAPI(title="SKMS Backend")
 
-app.include_router(item_listing_router)
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
