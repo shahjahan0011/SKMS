20c0a5841bec6dbebaf45c74b9fa5c84e57fa010
natsofek <62151299+natsofek@users.noreply.github.com>
2026-03-11 01:35:52 -0700

    Fix: registered auth_router to resolve 404s

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,15 @@
 """FastAPI Application"""
 
 from fastapi import FastAPI
-
+from app.routers.auth_router import router as auth_router
 from app.routers.item_listing_routers import router as item_listing_router
 from app.routers.restaurants_routers import router as restaurants_router
 from app.routers.menu_routers import router as menu_router
 
 app = FastAPI(title="SKMS Backend")
 
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+
 app.include_router(item_listing_router)
 app.include_router(restaurants_router)
 app.include_router(menu_router)
-
