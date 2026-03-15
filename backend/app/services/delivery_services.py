31b525c6f7c51df28bc15cc0b7d53e86546d5179
Khushi Kishan <khushi.kishan2004@gmail.com>
2026-03-15 12:22:35 -0700

    returns delivery by order id

--- backend/app/services/delivery_services.py
+++ backend/app/services/delivery_services.py
@@ -11,10 +11,21 @@ class delivery_services:
         self.repo = delivery_repository()
 
     
+
     def get_all_deliveries(self):
         """returns all deliveries"""
         deliveries = self.repo.get_all_deliveries()
         return deliveries
     
+
+    def get_delivery_by_order_id(self, order_id):
+        """returns delivery for an order by order id"""
+
+        delivery = self.repo.get_delivery_by_order_id(order_id)
+
+        if delivery is None:
+            raise ValueError("delivery not found")
+
+        return delivery
 
     
