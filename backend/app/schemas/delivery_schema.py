495b053f5b55dc99dd9950ebbdc5354cb77f05a8
Khushi Kishan <khushi.kishan2004@gmail.com>
2026-03-14 18:32:24 -0700

    added delivery schema for delivery info

---
+++ backend/app/schemas/delivery_schema.py
@@ -0,0 +1,17 @@
+"""Delivery Schema"""
+
+from pydantic import BaseModel
+from app.schemas.location_schema import location
+
+class delivery(BaseModel):
+    """
+    Delivery Information Entity
+    """
+
+    order_id: int
+    restaurant_id: int
+    user_id: int
+    user_name: str
+    delivery_location: location
+    status: str
+    is_Emergency: bool
