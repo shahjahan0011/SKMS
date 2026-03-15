b2d0125860612ba22e96381b190d0520f2f90d36
Jahan Shah <85148797+shahjahan0011@users.noreply.github.com>
2026-03-14 22:27:02 -0700

    Register cost router in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI
 from app.routers.item_listing_routers import router as item_listing_router
 from app.routers.auth_router import router as auth_router
 from app.routers.order_router import router as order_router
+from app.routers.cost_router import router as cost_router
 
 app = FastAPI(title="SKMS Backend")
 
 app.include_router(item_listing_router)
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 app.include_router(order_router)
+app.include_router(cost_router)
