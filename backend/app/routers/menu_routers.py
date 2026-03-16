7222c71ea981aad1193956d3a3ca153b49e546c9
natsofek <62151299+natsofek@users.noreply.github.com>
2026-03-16 03:24:13 -0700

    Update menu_routers.py

--- backend/app/routers/menu_routers.py
+++ backend/app/routers/menu_routers.py
@@ -1,7 +1,7 @@
 """Menu routes module."""
 
 from typing import Any, Dict, Optional
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, Path 
 
 from app.services.menu_services import MenuService
 from app.storage.repositories.menu_repository import menu_repository
@@ -10,12 +10,13 @@ from app.storage.repositories.restaurant_repository import restaurant_repository
 router = APIRouter(tags=["Menus"])
 
 def get_menu_service():
-    """Dependency injection for MenuService. """
+    """Dependency injection for MenuService."""
     return MenuService(menu_repository(), restaurant_repository())
 
 @router.get("/", response_model=Dict[str, Any])
 def get_menu_by_restaurant(
-    restaurant_id: str,
+
+    restaurant_id: str = Path(..., description="The ID of the restaurant"),
     search: Optional[str] = Query(None),
     page: int = Query(1, ge=1),
     page_size: int = Query(10, ge=1),
