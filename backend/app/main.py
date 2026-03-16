5694cef28ee98c5a6d06382bb1c3394a37e238c3
natsofek <62151299+natsofek@users.noreply.github.com>
2026-03-16 05:28:26 -0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,12 +11,12 @@ from app.routers.delivery_router import router as delivery_router
 
 app = FastAPI(title="SKMS Backend", redirect_slashes=True)
 
+app.include_router(delivery_router, tags=["Deliveries"])
+app.include_router(menu_router, tags=["Menus"])
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 app.include_router(order_router)
 app.include_router(item_listing_router)
-app.include_router(menu_router, tags=["Menus"])
 app.include_router(restaurants_router, prefix="/restaurants", tags=["Restaurants"])
-app.include_router(delivery_router, tags=["Deliveries"])
 app.include_router(delivery_router, prefix="/locations", tags=["Locations"])
 app.include_router(item_listing_router, tags=["Restaurants"])
 
