77f0e87eb9242ceb65d3061dd8d46fae35790586
Khushi Kishan <khushi.kishan2004@gmail.com>
2026-03-24 16:48:06 -0700

    cleaned item listing router test

--- backend/tests/test_item_listing_routers.py
+++ backend/tests/test_item_listing_routers.py
@@ -8,47 +8,59 @@ client = TestClient(app)
 
 
 def test_get_all_restaurants():
-    """Test for GET /restaurants endpoint"""
+    """test for getting restaurants endpoint"""
     response = client.get("/restaurants")
     print(f"DEBUG RESPONSE: {response.json()}")
     assert response.status_code == 200
     assert isinstance(response.json()["data"], list)
 
+
+
 def test_get_restaurant_by_id_valid():
-    """Test for GET /restaurants/{restaurant_id} endpoint for valid id"""
+    """test for getting a valid restaurant id"""
     response = client.get("/restaurants/1")
 
     assert response.status_code == 200
     assert response.json()["id"] == "1"
 
+
+
 def test_get_restaurant_by_id_invalid():
-    """Test for GET /restaurants/{restaurant_id} endpoint for invalid id"""
+    """test for getting an invalid restaurant id"""
     response = client.get("/restaurants/999999")
 
     assert response.status_code == 404
 
+
+
 def test_get_restaurant_menu_valid():
-    """Test for GET /restaurants/{restaurant_id}/menu endpoint with valid id"""
+    """test for getting a valid menu for a restaurant"""
     response = client.get("/restaurants/1/menu")
 
     assert response.status_code == 200
     assert isinstance(response.json().get("items", []), list)
 
+
+
 def test_get_restaurant_menu_invalid():
-    """Test for GET /restaurants/{restaurant_id}/menu endpoint with invalid id"""
+    """test for getting an invalid menu for a restaurant"""
     response = client.get("/restaurants/999999/menu")
 
     assert response.status_code == 404
 
+
+
 def test_get_menu_item_by_id_valid():
-    """Test for GET /menu/{item_id} endpoint with valid id"""
+    """test for getting a valid menu item by id"""
     response = client.get("/menu/1")
 
     assert response.status_code == 200
     assert response.json()["id"] == "1"
 
+
+
 def test_get_menu_item_by_id_invalid():
-    """Test for GET /menu/{item_id} endpoint with invalid id"""
+    """test for getting an invalid menu item by id"""
     response = client.get("/menu/999999")
 
     assert response.status_code == 404
